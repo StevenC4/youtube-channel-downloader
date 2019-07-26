4dfb16d4a1b7186f5018ab32e7804aaf74169a65
Steve Carroll <scarroll@qualtrics.com>
2019-07-25 21:33:41 -0600

    Adding a basic page with a home and channels page

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, NavLink} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <nav>
+          <ul>
+            <li>
+              <NavLink to='/' exact activeClassName='activeLink'>Home</NavLink>
+            </li>
+            <li>
+              <NavLink to='/channels' activeClassName='activeLink'>Channels</NavLink>
+            </li>
+          </ul>
+        </nav>
+        <Route path='/' exact component={Index}/>
+        <Route path='/channels' component={Channels}/>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+function Index() {
+  return (
+    <div>
+      <h2>Index page</h2>
+      <p>Basic stats about numbers of videos and channels you've subscribed to</p>
+    </div>
+  );
+}
+
+function Channels({match}) {
+  return (
+    <div>
+      <h2>Channels page</h2>
+      <Route path={`${match.path}/:id`} component={ChannelViewer} />
+      <Route path={match.path} exact component={ChannelList} />
+    </div>
+    );
+}
+
+function ChannelList() {
+  return (
+    <div>
+      <h3>List of channels</h3>
+    </div>
+  );
+}
+
+function ChannelViewer({match}) {
+  return (
+    <div>
+      <h3>Channel viewer: {match.params.id}</h3>
+      <p3>Video list</p3>
     </div>
   );
 }
