966d217d5aea3e4da46eb889ef00c0d0514d24e2
Steve Carroll <stevencarroll90@gmail.com>
2019-10-15 22:26:06 -0600

    Added text filtering and sorting for channels list

--- client/src/components/Channel/ChannelList.js
+++ client/src/components/Channel/ChannelList.js
@@ -1,23 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import moment from 'moment';
 import { NavLink } from 'react-router-dom';
 
 const ChannelList = props => {
+	const [filterText, setFilterText] = useState('');
 	const handleClick = _e => props.setModalOpenState(true, 'Select a channel to download');
-
-	// TODO: Sort channels by alphabetical order
+	const handleChange = e => setFilterText(e.target.value);
+	const channelIds = [...props.channels.ids]
+		.filter(channelId => props.channels.byId[channelId].name.toLowerCase().indexOf(filterText.toLowerCase()) !== -1)
+		.sort((a, b) => props.channels.byId[a].name.localeCompare(props.channels.byId[b].name))
 	return (
 		<div>
 			<nav className='tile-container'>
 				<div className='utilities'>
-					<label>
-						Filter:
-						<input className='filter' type='text'></input>
-					</label>
-					<div className='addButton' onClick={handleClick}>+ Add Channel</div>
+					<div className='spacer'></div>
+					<input className='filter' type='text' placeholder='Filter channels...' value={filterText} onChange={handleChange}></input>
+					<div className='spacer'>
+						<div className='addButton' onClick={handleClick}>+ Add Channel</div>
+					</div>
 				</div>
 				<div className='tiles'> 
-					{props.channels.ids.map(channelId =>
+					{channelIds.map(channelId =>
 						<div key={channelId} className='tile'>
 							<NavLink to={`${props.match.url}/${channelId}`}>
 								<div className='title'>{props.channels.byId[channelId].name}</div>
